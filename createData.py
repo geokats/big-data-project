409c676d8a6d9aa63c093e11d59005ec85ef4097
geokats <geo.katsogiannis@gmail.com>
2021-04-03 02:12:10 +0300

    Add base for createData

---
+++ createData.py
@@ -0,0 +1,19 @@
+def createData(keyFile, nLines, nesting, nkeys, maxLength):
+    """
+    Generates syntactically correct data for the KV Broker
+
+    Parameters:
+        keyFile (string): the path to a file containing a space-separated list of keys and their data types
+        nlines (int): the number of lines to generate
+        nesting (int): the maximum nesting level
+        nkeys (int): the maximum number of keys inside each value
+        maxLength (int): the maximum length of a string value
+    """
+
+    for n in nLines:
+        for
+
+
+
+if __name__ == '__main__':
+    createData()
