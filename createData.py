4b90f1486a048603d086a4f88c50baa99fc77a54
geokats <geo.katsogiannis@gmail.com>
2021-04-04 00:52:58 +0300

    Add first working implementation of createData

--- createData.py
+++ createData.py
@@ -1,19 +1,42 @@
-def createData(keyFile, nLines, nesting, nkeys, maxLength):
+import random
+
+NEST_PROBA = 0.1
+
+def createData(keyFile, nLines, nesting, nKeys, maxLength):
     """
     Generates syntactically correct data for the KV Broker
 
     Parameters:
         keyFile (string): the path to a file containing a space-separated list of keys and their data types
-        nlines (int): the number of lines to generate
+        nLines (int): the number of lines to generate
         nesting (int): the maximum nesting level
-        nkeys (int): the maximum number of keys inside each value
+        nKeys (int): the maximum number of keys inside each value
         maxLength (int): the maximum length of a string value
     """
 
-    for n in nLines:
-        for
+    #Read key file
+    keys = {"name": "string",
+            "age" : "int"
+            }
 
+    for i in range(nLines):
+        line = "\"key{}\" : ".format(i) + "{"
 
+        for key in range(random.randrange(nKeys)):
+            if key > 0:
+                line += " ; "
+
+            keyName, keyType = random.choice(list(keys.items()))
+
+            if nesting > 0 and random.random() < NEST_PROBA:
+                for data in createData(keyFile, nLines, nesting-1, nKeys, maxLength):
+                    line += data
+            else:
+                line += " \"{}\" : {}".format(keyName, keyType)
+
+        line += " }"
+        yield line
 
 if __name__ == '__main__':
-    createData()
+    for line in createData(None, 3, 1, 5, 10):
+        print(line)
