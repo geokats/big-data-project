9fdab4c3ec2e8e3f4e6ff8290eedb8eae29245a0
geokats <geo.katsogiannis@gmail.com>
2021-04-13 00:12:08 +0300

    Add simple communication

--- kvBroker.py
+++ kvBroker.py
@@ -1,6 +1,5 @@
 import argparse
-
-
+import socket
 
 if __name__ == '__main__':
     #Parse arguments
@@ -17,3 +16,19 @@ if __name__ == '__main__':
                         will have the same replicated data'
                         )
     args = parser.parse_args()
+
+    #Read server server file
+    servers = []
+    with open(args.s, "r") as f:
+        for line in f:
+            line = line.rstrip("\n")
+            address, port = line.split(" ")
+            servers.append((address, int(port)))
+
+    for address, port in servers:
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.connect((address, port))
+            s.sendall(b'Hello, world')
+            data = s.recv(1024)
+
+        print('Received', repr(data))
--- kvServer.py
+++ kvServer.py
@@ -1,6 +1,5 @@
 import argparse
-
-
+import socket
 
 if __name__ == '__main__':
     #Parse arguments
@@ -12,3 +11,21 @@ if __name__ == '__main__':
                         help='KV server\'s port'
                         )
     args = parser.parse_args()
+
+    #Create and bind socket
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind((args.a, args.p))
+
+    s.listen()
+
+    conn, addr = s.accept()
+    with conn:
+        print('Connected by', addr)
+        while True:
+            data = conn.recv(1024)
+            if not data:
+                break
+            conn.sendall(data)
+
+    #Close socket
+    s.close()
