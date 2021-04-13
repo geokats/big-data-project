da171bb40a33197f93471efe6d82b88e4a8a9ec8
geokats <geo.katsogiannis@gmail.com>
2021-04-13 21:02:04 +0300

    Add insertion of dictionary with nested trie

--- trie.py
+++ trie.py
@@ -10,7 +10,16 @@ class Node(object):
     def insert(self, key, value):
         if len(key) == 0:
             #If the key is empty the value must be inserted in this node
-            self.value = value
+            if isinstance(value, dict):
+                #If the value is a dictionary, we must construct a sub-trie
+                n = Node(key="")
+                for nk, nv in value.items():
+                    n.insert(nk, nv)
+
+                self.value = n
+            else:
+                #If the value is not a dictionary we can just insert it
+                self.value = value
         elif len(self.children) == 0:
             #If this node has no children create one with the given key and value
             newNode = Node(key[0])
@@ -58,8 +67,15 @@ if __name__ == '__main__':
     n = Node("")
     n.insert("abc", 42)
     n.insert("abc", 22)
-    n.insert("abcd", Node(""))
+    n.insert("abcd", {"key1": 1, "key2": "aaa", "key3": 20.2})
+    n.insert("la", {"key": 1, "ke": {"foo":100}})
+
     print(n.find("abc"))
     n.delete("abc")
     print(n.find("abc"))
+
     print(n.find("abcd"))
+    print(n.find("abcd").find("key1"))
+    print(n.find("abcd").find("key2"))
+    print(n.find("abcd").find("key3"))
+    print(n.find("la").find("ke").find("foo"))
