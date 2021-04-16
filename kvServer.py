1443af0f8a9a6889c5ca81a51d1c8303d6ce4fef
geokats <geo.katsogiannis@gmail.com>
2021-04-16 16:49:22 +0300

    Add documentation and comments to kvServer

--- kvServer.py
+++ kvServer.py
@@ -4,6 +4,16 @@ import json
 from trie import Node
 
 def parseKV(kv):
+    """
+    Parses a key-value pair by converting it into json format and using python's
+    json libarary
+        Parameters:
+            kv (string): A string describing a key-value pair as is defined for
+                the means of this project
+        Returns:
+            key (string): the key of the key-value pair
+            value : the value of the key-value pair in its appropriate type
+    """
     j = kv.replace(";",",")
     d = json.loads("{{{}}}".format(j))
     assert len(d) == 1
@@ -30,7 +40,7 @@ if __name__ == '__main__':
     s.bind((args.a, args.p))
     s.listen()
 
-    #KeyValue insertion phase
+    #Set-up phase
     stop_cmd = False
     while not stop_cmd:
         conn, addr = s.accept()
@@ -68,8 +78,10 @@ if __name__ == '__main__':
             if msg == "STOP":
                 stop_cmd = True
             elif msg == "CHECK":
+                #If the broker checks for the server's availability, reply with OK
                 conn.sendall(b"OK")
             elif msg.startswith("GET"):
+                #Look if the key exists as a top-level key only
                 key = msg.lstrip("GET ")
                 result = store.find(key)
                 if result is None:
@@ -79,6 +91,7 @@ if __name__ == '__main__':
                 conn.sendall(reply)
 
             elif msg.startswith("QUERY"):
+                #Split the key into subkeys and check for each subkey while moving deeper
                 key = msg.lstrip("QUERY ")
                 subkeys = key.split(".")
 
@@ -96,6 +109,7 @@ if __name__ == '__main__':
                 conn.sendall(reply)
 
             elif msg.startswith("DELETE"):
+                #If the key is found and deleted reply with OK, otherwise NOT FOUND
                 key = msg.lstrip("DELETE ")
                 if store.delete(key):
                     conn.sendall(b"OK")
@@ -103,8 +117,6 @@ if __name__ == '__main__':
                     conn.sendall(b"NOT FOUND")
             else:
                 conn.sendall(b"ERROR")
-
-
 
     #Close socket
     s.close()
