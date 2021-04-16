b2b237a84d120303451df6a4ddac1b1182e9b870
geokats <geo.katsogiannis@gmail.com>
2021-04-16 16:16:44 +0300

    Add documentation for trie

--- trie.py
+++ trie.py
@@ -2,12 +2,32 @@ class Node(object):
     """A trie node"""
 
     def __init__(self, key, value=None):
+        """
+        Initialize the trie node.
+            Parameters:
+                key (string): The key of the node is a single character. If a longer
+                    key is used, errors might be caused. A root node can have a longer
+                    key because it is never used.
+                value: The value that the node will store.
+        """
         super(Node, self).__init__()
         self.children = []
         self.key = key
         self.value = value
 
     def insert(self, key, value):
+        """
+        Insert a key-value instance in the trie. If the key is an empty string,
+        the value  is inserted in this node. Otherwise, the value will be recurrently
+        propagated deeper in the trie until each letter of the key is assigned to
+        a node and the deepest node will contain the value.
+        If the value is a dictionary, a new trie will be created containing
+        the key-value pairs of the dictionary. The new trie will be assigned
+        as a value at the appropriate node.
+            Parameters:
+                key (string): The key under which the value will be stored
+                value: The value that the trie will store.
+        """
         if len(key) == 0:
             #If the key is empty the value must be inserted in this node
             if isinstance(value, dict):
@@ -44,6 +64,17 @@ class Node(object):
                     break
 
     def find(self, key):
+        """
+        Searches for the given key in the trie. If the key is an empty string
+        then the value of the current node is returned. Otherwise the first letter
+        of the key is searched in the node's children and the rest of the key
+        is searched recurrently in the appropriate child.
+            Parameters:
+                key (string): The key to search for.
+            Return:
+                None if the value is not found, otherwise returns the corresponding
+                value
+        """
         if len(key) == 0:
             return self.value
         else:
@@ -53,6 +84,14 @@ class Node(object):
         return None
 
     def delete(self, key):
+        """
+        Searches for the given key in the trie and sets its value to None.
+        Works similarly to the Node.find() method
+            Parameters:
+                key (string): The key to delete.
+            Return:
+                True if the value was found and deleted, otherwise False
+        """
         if len(key) == 0:
             if self.value != None:
                 self.value = None
@@ -66,6 +105,18 @@ class Node(object):
         return False
 
     def _print(self, key_prefix):
+        """
+        A reccurrent helping method for creating a string representation of the
+        trie
+            Parameters:
+                key_prefix (string): The key gathered from the previously
+                    traversed nodes
+            Return:
+                ret (dictionary): A dictionary containing all the key-value
+                pairs of the node
+                ret_str (string): A printable string version of the afforementioned
+                dictionary
+        """
         ret = {}
         cur_key = key_prefix + self.key
 
@@ -89,6 +140,10 @@ class Node(object):
         return ret, ret_str
 
     def __str__(self):
+        """
+        Return a printable string representation of the trie, using the
+        reccurent Node._print() method
+        """
         ret, ret_str = self._print("")
         return ret_str
 
